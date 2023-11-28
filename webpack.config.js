d3331ae8cf4054a6f6e5051390687ac37861eb77
Stiven Gomez <stivengomez@soyyo.co>
2023-11-28 10:31:22 -0500

    REFACTOR - Refactorización de webpack.config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 
+const rulesForJavaScript = {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    options: {
+        presets: [
+            [
+                '@babel/preset-react',
+                {
+                    runtime: 'automatic' // classic
+                }
+            ]
+        ]
+    }
+}
+
+const rules = [ rulesForJavaScript ]
 
 module.exports = {
     // entry: '',
     output: {
         path: path.resolve(__dirname,  'build')
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        [
-                            '@babel/preset-react',
-                            {
-                                runtime: 'automatic' // classic
-                            }
-                        ]
-                    ]
-                }
-            }
-        ]
-    }
+    module: { rules }
 }
